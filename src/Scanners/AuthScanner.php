7c5550b06fede2a3a3480c1b98bc41a152eaecbe
Nextgenthing (AI Bot) <133497161+nextgenthing@users.noreply.github.com>
2023-07-14 20:10:52 +0100

    Update AuthScanner.php

--- src/Scanners/AuthScanner.php
+++ src/Scanners/AuthScanner.php
@@ -8,30 +8,38 @@ class AuthScanner
 {
     public function scanAuthentication()
     {
-        if ($this->isWeakPasswordHashingUsed()) {
-            $vulnerabilityDetails = [
-                'type' => 'Authentication',
-                'severity' => 'Medium',
-                'description' => 'Weak password hashing algorithm detected.'
-            ];
+        $this->checkPasswordHashing();
+        // Add more authentication scanning methods as needed
+    }
 
-            // Store the vulnerability details or generate a report
-            $this->storeVulnerability($vulnerabilityDetails);
+    private function checkPasswordHashing()
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            if ($this->isWeakPasswordHashingUsed($user->password)) {
+                $vulnerabilityDetails = [
+                    'type' => 'Authentication',
+                    'severity' => 'Medium', // Adjust severity level based on your assessment
+                    'description' => 'Weak password hashing algorithm detected for user: ' . $user->email
+                ];
+
+                $this->storeVulnerability($vulnerabilityDetails);
+            }
         }
     }
 
-    private function isWeakPasswordHashingUsed()
+    private function isWeakPasswordHashingUsed($hashedPassword)
     {
         // Implement the authentication vulnerability scanning logic here
         // Check if weak password hashing algorithms like MD5 or SHA1 are used
 
-        // Example: Check if the user's password is hashed using MD5
-        $hashedPassword = Auth::user()->password;
-        return strpos($hashedPassword, 'md5') !== false;
+        // Example: Check if the user's password is hashed using MD5 or SHA1
+        return preg_match('/^\$[1|5]\$/', $hashedPassword);
     }
 
     private function storeVulnerability($vulnerabilityDetails)
     {
-        // Implement the storage or reporting logic for authentication vulnerabilities
+        // Implement the logic to store or report the detected vulnerability
     }
 }
