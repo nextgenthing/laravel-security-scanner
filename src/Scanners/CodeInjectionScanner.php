bb0fe864d10cde62294376d37ba9790c63abb8f5
Nextgenthing (AI Bot) <133497161+nextgenthing@users.noreply.github.com>
2023-07-14 20:19:48 +0100

    Update CodeInjectionScanner.php

--- src/Scanners/CodeInjectionScanner.php
+++ src/Scanners/CodeInjectionScanner.php
@@ -2,34 +2,96 @@
 
 namespace Your\Package\Namespace;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Notification;
+use Your\Package\Namespace\Models\Vulnerability;
+use Your\Package\Namespace\Notifications\VulnerabilityDetectedNotification;
+
 class CodeInjectionScanner
 {
-    public function scanCodeInjection($code)
+    public function scanCodeInjection()
     {
-        if ($this->isCodeInjectionDetected($code)) {
-            $vulnerabilityDetails = [
-                'type' => 'Code Injection',
-                'code' => $code,
-                'severity' => 'High',
-                'description' => 'Potential code injection vulnerability detected.'
-            ];
+        $this->checkDynamicQueries();
+        $this->checkEvalFunction();
+        // Add more code injection scanning methods as needed
+    }
 
-            // Store the vulnerability details or generate a report
-            $this->storeVulnerability($vulnerabilityDetails);
+    private function checkDynamicQueries()
+    {
+        $routes = Route::getRoutes();
+
+        foreach ($routes as $route) {
+            $routeAction = $route->getAction();
+            $controller = $routeAction['controller'];
+
+            if (is_string($controller) && $this->containsDynamicQuery($controller)) {
+                $vulnerabilityDetails = [
+                    'type' => 'Code Injection',
+                    'severity' => 'High',
+                    'description' => 'Potential dynamic SQL query detected in controller: ' . $controller,
+                ];
+
+                $this->storeVulnerability($vulnerabilityDetails);
+            }
         }
     }
 
-    private function isCodeInjectionDetected($code)
+    private function containsDynamicQuery($code)
     {
         // Implement the code injection vulnerability scanning logic here
-        // Check if the code contains potentially malicious patterns or performs insecure code execution
+        // Check if the code contains dynamic queries or user-controlled input
 
-        // Example: Check if the code contains 'exec(' indicating potential command injection
-        return strpos($code, 'exec(') !== false;
+        // Example: Check if the code contains the string "select" followed by a variable or user input
+        return preg_match('/select\s+\$|select\s+\$\{|select\s+\$[a-z0-9_]+/i', $code);
+    }
+
+    private function checkEvalFunction()
+    {
+        $routes = Route::getRoutes();
+
+        foreach ($routes as $route) {
+            $routeAction = $route->getAction();
+            $controller = $routeAction['controller'];
+
+            if (is_string($controller) && $this->containsEvalFunction($controller)) {
+                $vulnerabilityDetails = [
+                    'type' => 'Code Injection',
+                    'severity' => 'High',
+                    'description' => 'Potential eval() function detected in controller: ' . $controller,
+                ];
+
+                $this->storeVulnerability($vulnerabilityDetails);
+            }
+        }
+    }
+
+    private function containsEvalFunction($code)
+    {
+        // Implement the code injection vulnerability scanning logic here
+        // Check if the code contains the eval() function
+
+        // Example: Check if the code contains the string "eval("
+        return strpos($code, 'eval(') !== false;
     }
 
     private function storeVulnerability($vulnerabilityDetails)
     {
-        // Implement the storage or reporting logic for code injection vulnerabilities
+        // Implement the logic to store or report the detected vulnerability
+        // You can customize this method based on your requirements and preferences
+        // For example, you can store the vulnerabilities in a database, log them, or send notifications
+        
+        // Example: Store vulnerabilities in a database table named 'vulnerabilities'
+        $vulnerability = new Vulnerability();
+        $vulnerability->type = $vulnerabilityDetails['type'];
+        $vulnerability->severity = $vulnerabilityDetails['severity'];
+        $vulnerability->description = $vulnerabilityDetails['description'];
+        $vulnerability->save();
+        
+        // Example: Log the vulnerability details
+        Log::info('Vulnerability detected: ' . $vulnerabilityDetails['description']);
+        
+        // Example: Send a notification to an admin or security team
+        Notification::send($adminUsers, new VulnerabilityDetectedNotification($vulnerabilityDetails));
     }
 }
