425254cefbd711efbbaebd94c06edcdaf0006a87
Nextgenthing (AI Bot) <133497161+nextgenthing@users.noreply.github.com>
2023-07-14 20:24:23 +0100

    Update FileInclusionScanner.php

--- src/Scanners/FileInclusionScanner.php
+++ src/Scanners/FileInclusionScanner.php
@@ -2,34 +2,66 @@
 
 namespace Your\Package\Namespace;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Notification;
+use Your\Package\Namespace\Models\Vulnerability;
+use Your\Package\Namespace\Notifications\VulnerabilityDetectedNotification;
+
 class FileInclusionScanner
 {
-    public function scanFileInclusions($filePath)
+    public function scanFileInclusion()
     {
-        if ($this->isInsecureFilePath($filePath)) {
-            $vulnerabilityDetails = [
-                'type' => 'File Inclusion',
-                'file_path' => $filePath,
-                'severity' => 'High',
-                'description' => 'Potential file inclusion vulnerability detected.'
-            ];
+        $this->checkFileInclusion();
+        // Add more file inclusion scanning methods as needed
+    }
 
-            // Store the vulnerability details or generate a report
-            $this->storeVulnerability($vulnerabilityDetails);
+    private function checkFileInclusion()
+    {
+        $routes = Route::getRoutes();
+
+        foreach ($routes as $route) {
+            $routeAction = $route->getAction();
+            $controller = $routeAction['controller'];
+
+            if (is_string($controller) && $this->containsFileInclusion($controller)) {
+                $vulnerabilityDetails = [
+                    'type' => 'File Inclusion',
+                    'severity' => 'High',
+                    'description' => 'Potential file inclusion vulnerability detected in controller: ' . $controller,
+                ];
+
+                $this->storeVulnerability($vulnerabilityDetails);
+            }
         }
     }
 
-    private function isInsecureFilePath($filePath)
+    private function containsFileInclusion($code)
     {
         // Implement the file inclusion vulnerability scanning logic here
-        // Check if the file path contains insecure patterns or performs insecure file inclusion operations
+        // Check if the code contains potential file inclusion vulnerabilities
 
-        // Example: Check if the file path contains '../' indicating directory traversal vulnerability
-        return strpos($filePath, '../') !== false;
+        // Example: Check if the code contains the string "include(" or "require("
+        return strpos($code, 'include(') !== false || strpos($code, 'require(') !== false;
     }
 
     private function storeVulnerability($vulnerabilityDetails)
     {
-        // Implement the storage or reporting logic for file inclusion vulnerabilities
+        // Implement the logic to store or report the detected vulnerability
+        // You can customize this method based on your requirements and preferences
+        // For example, you can store the vulnerabilities in a database, log them, or send notifications
+        
+        // Example: Store vulnerabilities in a database table named 'vulnerabilities'
+        $vulnerability = new Vulnerability();
+        $vulnerability->type = $vulnerabilityDetails['type'];
+        $vulnerability->severity = $vulnerabilityDetails['severity'];
+        $vulnerability->description = $vulnerabilityDetails['description'];
+        $vulnerability->save();
+        
+        // Example: Log the vulnerability details
+        Log::info('Vulnerability detected: ' . $vulnerabilityDetails['description']);
+        
+        // Example: Send a notification to an admin or security team
+        Notification::send($adminUsers, new VulnerabilityDetectedNotification($vulnerabilityDetails));
     }
 }
