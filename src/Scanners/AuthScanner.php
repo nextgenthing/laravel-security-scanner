5a072fe5607e64e9638668f26a8ec72898af9c6b
Nextgenthing (AI Bot) <133497161+nextgenthing@users.noreply.github.com>
2023-07-14 20:18:30 +0100

    Update AuthScanner.php

--- src/Scanners/AuthScanner.php
+++ src/Scanners/AuthScanner.php
@@ -3,16 +3,21 @@
 namespace Your\Package\Namespace;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Notification;
+use Your\Package\Namespace\Models\Vulnerability;
+use Your\Package\Namespace\Notifications\VulnerabilityDetectedNotification;
 
 class AuthScanner
 {
     public function scanAuthentication()
     {
-        $this->checkPasswordHashing();
-        // Add more authentication scanning methods as needed
+        $this->checkWeakPasswordHashing();
+        $this->checkRememberTokenExpiration();
+        $this->checkInactiveUserSessions();
     }
 
-    private function checkPasswordHashing()
+    private function checkWeakPasswordHashing()
     {
         $users = User::all();
 
@@ -20,8 +25,8 @@ class AuthScanner
             if ($this->isWeakPasswordHashingUsed($user->password)) {
                 $vulnerabilityDetails = [
                     'type' => 'Authentication',
-                    'severity' => 'Medium', // Adjust severity level based on your assessment
-                    'description' => 'Weak password hashing algorithm detected for user: ' . $user->email
+                    'severity' => 'Medium',
+                    'description' => 'Weak password hashing algorithm detected for user: ' . $user->email,
                 ];
 
                 $this->storeVulnerability($vulnerabilityDetails);
@@ -34,12 +39,67 @@ class AuthScanner
         // Implement the authentication vulnerability scanning logic here
         // Check if weak password hashing algorithms like MD5 or SHA1 are used
 
-        // Example: Check if the user's password is hashed using MD5 or SHA1
-        return preg_match('/^\$[1|5]\$/', $hashedPassword);
+        // Example: Check if the user's password is hashed using MD5
+        return strpos($hashedPassword, 'md5') !== false;
+    }
+
+    private function checkRememberTokenExpiration()
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            if ($this->isRememberTokenExpired($user->remember_token)) {
+                $vulnerabilityDetails = [
+                    'type' => 'Authentication',
+                    'severity' => 'High',
+                    'description' => 'Expired remember token detected for user: ' . $user->email,
+                ];
+
+                $this->storeVulnerability($vulnerabilityDetails);
+            }
+        }
+    }
+
+    private function isRememberTokenExpired($rememberToken)
+    {
+        // Implement the logic to check if the remember token has expired
+
+        // Example: Check if the remember token is null or empty
+        return empty($rememberToken);
+    }
+
+    private function checkInactiveUserSessions()
+    {
+        $inactiveUsers = User::where('last_activity', '<=', now()->subMonths(6))->get();
+
+        foreach ($inactiveUsers as $user) {
+            $vulnerabilityDetails = [
+                'type' => 'Authentication',
+                'severity' => 'Low',
+                'description' => 'Inactive user session detected for user: ' . $user->email,
+            ];
+
+            $this->storeVulnerability($vulnerabilityDetails);
+        }
     }
 
     private function storeVulnerability($vulnerabilityDetails)
     {
         // Implement the logic to store or report the detected vulnerability
+        // You can customize this method based on your requirements and preferences
+        // For example, you can store the vulnerabilities in a database, log them, or send notifications
+        
+        // Example: Store vulnerabilities in a database table named 'vulnerabilities'
+        $vulnerability = new Vulnerability();
+        $vulnerability->type = $vulnerabilityDetails['type'];
+        $vulnerability->severity = $vulnerabilityDetails['severity'];
+        $vulnerability->description = $vulnerabilityDetails['description'];
+        $vulnerability->save();
+        
+        // Example: Log the vulnerability details
+        Log::info('Vulnerability detected: ' . $vulnerabilityDetails['description']);
+        
+        // Example: Send a notification to an admin or security team
+        Notification::send($adminUsers, new VulnerabilityDetectedNotification($vulnerabilityDetails));
     }
 }
