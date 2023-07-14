e5f4de6684f7ff1fafe91690ed6e48d585dd9ecf
Nextgenthing (AI Bot) <133497161+nextgenthing@users.noreply.github.com>
2023-07-14 20:23:27 +0100

    Update CsrfScanner.php

--- src/Scanners/CsrfScanner.php
+++ src/Scanners/CsrfScanner.php
@@ -2,40 +2,66 @@
 
 namespace Your\Package\Namespace;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Notification;
+use Your\Package\Namespace\Models\Vulnerability;
+use Your\Package\Namespace\Notifications\VulnerabilityDetectedNotification;
 
 class CsrfScanner
 {
-    public function scanRequest(Request $request)
+    public function scanCsrf()
     {
-        if ($this->isCsrfTokenRequired($request) && !$this->isCsrfTokenValid($request)) {
-            $vulnerabilityDetails = [
-                'type' => 'CSRF',
-                'url' => $request->url(),
-                'method' => $request->method(),
-                'severity' => 'High',
-                'description' => 'CSRF token validation failed.'
-            ];
+        $this->checkCsrfProtection();
+        // Add more CSRF scanning methods as needed
+    }
 
-            // Store the vulnerability details or generate a report
-            $this->storeVulnerability($vulnerabilityDetails);
+    private function checkCsrfProtection()
+    {
+        $routes = Route::getRoutes();
+
+        foreach ($routes as $route) {
+            $routeAction = $route->getAction();
+            $usesCsrf = $this->routeUsesCsrf($routeAction['uses']);
+
+            if (!$usesCsrf) {
+                $vulnerabilityDetails = [
+                    'type' => 'CSRF',
+                    'severity' => 'Medium',
+                    'description' => 'CSRF protection not enabled for route: ' . $route->uri(),
+                ];
+
+                $this->storeVulnerability($vulnerabilityDetails);
+            }
         }
     }
 
-    private function isCsrfTokenRequired(Request $request)
+    private function routeUsesCsrf($routeAction)
     {
-        return $request->isMethod('post') || $request->isMethod('put') || $request->isMethod('patch') || $request->isMethod('delete');
-    }
+        // Implement the CSRF scanning logic here
+        // Check if the route uses CSRF protection
 
-    private function isCsrfTokenValid(Request $request)
-    {
-        $token = $request->input('_token');
-        return Session::token() === $token;
+        // Example: Check if the route action contains the "csrf" middleware
+        return strpos($routeAction, 'csrf') !== false;
     }
 
     private function storeVulnerability($vulnerabilityDetails)
     {
-        // Implement the storage or reporting logic for CSRF vulnerabilities
+        // Implement the logic to store or report the detected vulnerability
+        // You can customize this method based on your requirements and preferences
+        // For example, you can store the vulnerabilities in a database, log them, or send notifications
+        
+        // Example: Store vulnerabilities in a database table named 'vulnerabilities'
+        $vulnerability = new Vulnerability();
+        $vulnerability->type = $vulnerabilityDetails['type'];
+        $vulnerability->severity = $vulnerabilityDetails['severity'];
+        $vulnerability->description = $vulnerabilityDetails['description'];
+        $vulnerability->save();
+        
+        // Example: Log the vulnerability details
+        Log::info('Vulnerability detected: ' . $vulnerabilityDetails['description']);
+        
+        // Example: Send a notification to an admin or security team
+        Notification::send($adminUsers, new VulnerabilityDetectedNotification($vulnerabilityDetails));
     }
 }
